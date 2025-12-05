741317ef9d983ec14e8096fcc6c5864515ba1b2a
givikuna <thommaster10@gmail.com>
2025-12-05 03:00:09 -0500

    1.0.8 hotfix

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import { Structure } from "./DataStructures/Structure";
-import * as Interface from "./Interfaces";
 import { Maths } from "./Maths/Maths";
 import { Sorting } from "./Sorting/Sorting";
 import { Util } from "./Utilities/Util";
 
 export class Encodex {
     public static Structure = Structure;
-    public static Interface = Interface;
     public static Maths = Maths;
     public static Sorting = Sorting;
     public static Util = Util;
