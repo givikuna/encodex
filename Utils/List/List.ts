df9d8879a5b8dfa2d7cf5559d4831bda22cca559
Larry <larryhe360@gmail.com>
2026-04-11 01:12:06 -0400

    added even more

--- Utils/List/List.ts
+++ Utils/List/List.ts
@@ -1,7 +1,7 @@
 export namespace List {
     export function map<T, U>(xs: T[], f: (x: T) => U): U[] {
         const a: U[] = [];
-        for(let i = 0; i < xs.length; i++){
+        for (let i = 0; i < xs.length; i++) {
             a.push(f(xs[i]));
         }
         return a;
@@ -9,19 +9,19 @@ export namespace List {
 
     export function tail<T>(xs: T[]): T[] {
         const a: T[] = [];
-        for(let i = 1; i < xs.length; i++){
+        for (let i = 1; i < xs.length; i++) {
             a.push(xs[i]);
         }
         return a;
     }
 
     export function last<T>(xs: T[]): T | undefined {
-        return xs.length == 0 ? undefined : xs[xs.length-1];
+        return xs.length == 0 ? undefined : xs[xs.length - 1];
     }
 
     export function initial<T>(xs: T[]): T[] {
         const a: T[] = [];
-        for(let i = 0; i < xs.length-1; i++){
+        for (let i = 0; i < xs.length - 1; i++) {
             a.push(xs[i]);
         }
         return a;
@@ -29,7 +29,7 @@ export namespace List {
 
     export function slice<T>(xs: T[], start: number, end: number = xs.length): T[] {
         const a: T[] = [];
-        for(let i = start; i < end; i++){
+        for (let i = start; i < end; i++) {
             a.push(xs[i]);
         }
         return a;
@@ -37,9 +37,50 @@ export namespace List {
 
     export function take<T>(xs: T[], n: number): T[] {
         const a: T[] = [];
-        for(let i = 0; i < n; i++){
+        for (let i = 0; i < n; i++) {
             a.push(xs[i]);
         }
         return a;
+    }
+
+    export function drop<T>(xs: T[], n: number): T[] {
+        const a: T[] = [];
+        for (let i = n; i < xs.length; i++) {
+            a.push(xs[i]);
+        }
+        return a;
+    }
+
+    export function split<T>(xs: T[], index: number): [T[], T[]] {
+        const a: T[] = [];
+        const b: T[] = [];
+        for (let i = 0; i < index; i++) {
+            a.push(xs[i]);
+        }
+        for (let i = index; i < xs.length; i++) {
+            b.push(xs[i]);
+        }
+        return [a, b];
+    }
+
+    export function span<T>(xs: T[], pred: (val: T) => boolean): [T[], T[]] {
+        const a: T[] = [];
+        const b: T[] = [];
+        for (let i = 0; i < xs.length; i++) {
+            if(pred(xs[i])){
+                a.push(xs[i]);
+                continue;
+            }
+            for (let j = i; j < xs.length; j++) {
+                b.push(xs[i]);
+            }
+            break;
+        }
+        return [a, b];
+    }
+
+    export function at<T>(xs: T[], index: number): T | undefined {
+        if(index >= xs.length || index < -xs.length) return undefined;
+        return (index < 0) ? xs[xs.length+index] : xs[index];
     }
 }
