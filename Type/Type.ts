3ffd0e009ff79eab861d760b3052445326aad82c
Givikuna <thommaster10@gmail.com>
2026-04-14 16:19:51 -0400

    Type module implemented

--- Type/Type.ts
+++ Type/Type.ts
@@ -1,3 +1,14 @@
 export namespace Type {
-    //
+    export type Nullable<T> = T | null;
+    export type ReversedArray<T extends any[]> = T extends [infer Head, ...infer Tail]
+        ? [...ReversedArray<Tail>, Head]
+        : [];
+    export type FlippedFunction<T> = T extends (...args: infer P) => infer R
+        ? (...args: ReversedArray<P>) => R
+        : never;
+    export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+    export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;
+    export type ElementOf<T> = T extends (infer U)[] ? U : never;
+    export type Awaitable<T> = T | Promise<T>;
+    export type If<T> = T extends true ? true : false;
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { Utils } from "./Utils/Utils";
+import { Type as _Type } from "./Type/Type";
 
 export class Encodex {
     public static Utils: typeof Utils = Utils;
+}
+
+export namespace Encodex {
+    export import Type = _Type;
 }
